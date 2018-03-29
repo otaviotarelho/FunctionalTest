5a3a67749297b183e174fed6bd92985c61e4f3c3
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-28 22:13:45 -0300

    First Modifications in TestChrome

--- src/main/java/cursoSelenium/TesteGoogle.java
+++ src/main/java/cursoSelenium/TesteGoogle.java
@@ -2,6 +2,8 @@ package cursoSelenium;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,10 +14,17 @@ public class TesteGoogle {
 	public void teste() {
 		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
 		WebDriver driver = new ChromeDriver();
+		
+		//Aula 21
+			// driver.manage().window().setPosition(new Point(100, 100)); window position
+			//driver.manage().window().setSize(new Dimension(1200, 765)); window size
+			driver.manage().window().maximize();
+		//
 		driver.get("http://www.google.com");
 		
 		Assert.assertEquals("Google", driver.getTitle());
 		
-		driver.close();
+		//driver.close(); fecha apenas o browser e não a instancia do chrome driver
+		driver.quit();
 	}
 }
