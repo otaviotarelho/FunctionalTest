248435709b89fed82fa2cb95301c13f9f7d398cd
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-29 22:19:15 -0300

    adding some elements texts

--- src/main/java/cursoSelenium/TestCampoTreinamento.java
+++ src/main/java/cursoSelenium/TestCampoTreinamento.java
@@ -1,19 +1,184 @@
 package cursoSelenium;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TestCampoTreinamento {
 	
 	
 	@Test
-	public void test() {
+	public void shouldWriteInTextField() {
 		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
 		
-		//driver.quit();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Texto a ser escrito");
+		Assert.assertEquals("Texto a ser escrito", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void shouldWriteInTextArea(){
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Texto a ser escrito no textArea\n\n outra linha \n\n ultima linha");
+		Assert.assertEquals("Texto a ser escrito no textArea\n\n outra linha \n\n ultima linha", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void shouldInteractWithRadio(){
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		
+		driver.quit();
+	}
+	
+
+	@Test
+	public void shouldInteractWithCheckbox(){
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:3")).isSelected());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void shouldSelectAnOptionInCombobox() {
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		
+		Select combo = new Select(element);
+		
+		//combo.selectByIndex(3);
+		//combo.selectByValue("superior");
+		combo.selectByVisibleText("2o grau completo");
+		
+		Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void shouldVerifyAvailableValuesFromCombo() {
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		
+		Select combo = new Select(element);
+		
+		List<WebElement> options = combo.getOptions();
+		
+		Assert.assertEquals(8, options.size()); //get combo size
+		
+		
+		boolean encontrou = false;
+		
+		for(WebElement option : options) {
+			if("Mestrado".equals(option.getText())) {
+				encontrou = true;
+				break;
+			}
+		}
+		
+		Assert.assertTrue(encontrou);
+		
+		encontrou = false;
+		for(WebElement option : options) {
+			if("Nova opt".equals(option.getText())) {
+				encontrou = true;
+				break;
+			}
+		}
+		
+		Assert.assertFalse(encontrou);
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void ShouldSelectMultipleCombo() {
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		
+		Select combo = new Select(element);
+		
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+		
+		Assert.assertEquals(3, allSelectedOptions.size());
+		
+		combo.deselectByVisibleText("Corrida");
+		allSelectedOptions = combo.getAllSelectedOptions();
+		
+		Assert.assertEquals(2, allSelectedOptions.size());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void ShouldClickSomeButtons() {
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		
+		WebElement findElement = driver.findElement(By.id("buttonSimple"));
+		
+		findElement.click();
+		
+		Assert.assertEquals("Obrigado!", findElement.getAttribute("value"));
+		
+		driver.quit();
+	}
+	
+	@Test
+	@Ignore
+	public void ShouldInterectWithAnchorWithoutId() {
+		System.setProperty("webdriver.chrome.driver","/Users/otaviortbarros/Developer/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///"+ System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		
+		driver.findElement(By.linkText("Voltar")).click();
+		
+		Assert.fail();
+		
+		driver.quit();
 	}
 }
